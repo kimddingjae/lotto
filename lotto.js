105da1035cb222e34981d6d28ec80cce1558fbfc
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2024-03-26 22:15:17 +0900

    Update lotto.js

--- lotto.js
+++ lotto.js
@@ -7,7 +7,7 @@ function decryptEffect(element, time) {
   const effect = setInterval(() => {
     element.innerText = Math.floor(Math.random() * 44 + 1);
   }, 10);
-  console.log("time    :::::::   " + time)
+  
   setTimeout(() => {
     const random = Math.floor(Math.random() * numbers.length);
 
@@ -15,7 +15,7 @@ function decryptEffect(element, time) {
     element.classList.add("done");
     element.innerText = numbers[random];
     numbers.splice(random, 1);
-  }, time * 1000 + 1000);
+  }, time * 500);
 }
 
 function lottery() {
@@ -80,7 +80,7 @@ document.getElementById("btn").addEventListener("click", function () {
   if (tmp === 1) {
     
     const cnt = document.getElementsByClassName("numbers").length;
-    console.log(cnt)
+    
     numbers = Array.from({ length: 45 }, (_, i) => i + 1);
     tmp = 0;
     btn.classList.add("hide");
