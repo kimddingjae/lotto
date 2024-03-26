a5076004af6f88d7a46d8baa308448e26cce9e6e
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2024-03-26 17:54:01 +0900

    Add files via upload

---
+++ lotto.js
@@ -0,0 +1,81 @@
+let tmp = 1;
+let numbers;
+const input_form = document.getElementById("input_form");
+const price = document.getElementById("price");
+const box = document.getElementById("box");
+function decryptEffect(element, time) {
+  const effect = setInterval(() => {
+    element.innerText = Math.floor(Math.random() * 44 + 1);
+  }, 10);
+
+  setTimeout(() => {
+    const random = Math.floor(Math.random() * numbers.length);
+
+    clearInterval(effect);
+    element.classList.add("done");
+    element.innerText = numbers[random];
+    numbers.splice(random, 1);
+  }, time * 1000 + 1000);
+}
+
+function lottery() {
+  document.querySelectorAll(".ball").forEach((element, index) => {
+    element.classList.remove("done");
+    decryptEffect(element, index);
+  });
+}
+
+document.getElementById("input").addEventListener("click", function () {
+  event.preventDefault();
+
+  const cnt = price.value / 5000;
+  for (var i = 0; i < cnt; i++) {
+    const container = document.createElement("div");
+    const ballBlue1 = document.createElement("div");
+    ballBlue1.classList += "ball blue";
+    ballBlue1.innerText = "?";
+    const ballBlue2 = document.createElement("div");
+    ballBlue2.classList += "ball blue";
+    ballBlue2.innerText = "?";
+    const ballBlue3 = document.createElement("div");
+    ballBlue3.classList += "ball blue";
+    ballBlue3.innerText = "?";
+    const ballRed1 = document.createElement("div");
+    ballRed1.classList += "ball red";
+    ballRed1.innerText = "?";
+    const ballRed2 = document.createElement("div");
+    ballRed2.classList += "ball red";
+    ballRed2.innerText = "?";
+    const ballGrey = document.createElement("div");
+    ballGrey.classList += "ball grey";
+    ballGrey.innerText = "?";
+    const plus = document.createElement("div");
+    plus.classList += "plus";
+    plus.innerText = "+";
+    const ballBonus = document.createElement("div");
+    ballBonus.classList += "ball bonus";
+    ballBonus.innerText = "?";
+    container.appendChild(ballBlue1);
+    container.appendChild(ballBlue2);
+    container.appendChild(ballBlue3);
+    container.appendChild(ballRed1);
+    container.appendChild(ballRed2);
+    container.appendChild(ballGrey);
+    container.appendChild(plus);
+    container.appendChild(ballBonus);
+    container.classList += "numbers";
+    box.appendChild(container);
+  }
+});
+
+document.getElementById("btn").addEventListener("click", function () {
+  if (tmp === 1) {
+    numbers = Array.from({ length: 45 }, (_, i) => i + 1);
+    tmp = 0;
+    btn.classList.add("hide");
+    lottery();
+    setTimeout(function () {
+      (tmp = 1), btn.classList.remove("hide");
+    }, 7500);
+  }
+});
